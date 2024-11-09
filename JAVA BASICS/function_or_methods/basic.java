97dc4df0807d24a83da60a37b41a441bc7cb57a7
karamfab <karamoberoi19@gmail.com>
2024-11-09 17:21:15 +0530

    functions and metods part 1

---
+++ JAVA BASICS/function_or_methods/basic.java
@@ -0,0 +1,11 @@
+
+public class basic {
+
+    
+    public static void printHelloworld() {
+        System.out.println("HEllo world");
+    }
+    public static void main(String[] args) {
+        printHelloworld();
+    }
+}
